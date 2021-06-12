b13d8f50f6c744c8e5f75333ccdffbf79aeaf9e4
jthcast <jthcast@gmail.com>
2021-06-12 20:33:45 +0900

    Add klaytn settings

---
+++ klaytn/KlaystagramContract.js
@@ -0,0 +1,7 @@
+import caver from './caver'
+
+const KlaystagramContract = DEPLOYED_ABI 
+  && DEPLOYED_ADDRESS
+  && new caver.klay.Contract(DEPLOYED_ABI, DEPLOYED_ADDRESS)
+
+export default KlaystagramContract
