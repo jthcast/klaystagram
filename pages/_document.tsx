5be7571a2659c8329d6d7fea6818fb71d3e967c2
jthcast <jthcast@gmail.com>
2021-06-13 16:01:20 +0900

    Add emotion settings

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import React from 'react';
+import { renderStatic } from '../styles/renderer'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const page = await ctx.renderPage();
+    const { css, ids } = await renderStatic(page.html);
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion={`css ${ids.join(' ')}`}
+            dangerouslySetInnerHTML={{ __html: css }}
+          />
+        </>
+      ),
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
