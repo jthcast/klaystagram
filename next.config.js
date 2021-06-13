dfd0105b21deaf949e9d1529f76712450a5de1fe
jthcast <jthcast@gmail.com>
2021-06-13 23:03:09 +0900

    Add webpack configure for deployed files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+const { GitRevisionPlugin } = require('git-revision-webpack-plugin')
+const fs = require('fs')
+
+const gitRevisionPlugin = new GitRevisionPlugin()
+
 module.exports = {
-  webpack: (config, { isServer }) => {
+  webpack: (config, { isServer, webpack }) => {
     if(!isServer){
       config.node = {
         fs: `empty`
       }
     }
+    config.plugins.push(new webpack.DefinePlugin({
+      DEV: false,
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.version': JSON.stringify(gitRevisionPlugin.commithash().slice(0, 7)), // TODO: delete when real
+      DEPLOYED_ADDRESS: JSON.stringify(fs.readFileSync('deployedAddress', 'utf8').replace(/\n|\r/g, "")),
+      DEPLOYED_ABI: fs.existsSync('deployedABI') && fs.readFileSync('deployedABI', 'utf8')
+    }))
 
     return config
   }
