aaafe3dfceda98b533c4127a2f99d259d025b238
jthcast <jthcast@gmail.com>
2021-06-13 16:04:59 +0900

    Add Auth component

---
+++ components/organisms/Auth.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import LoginForm from '../molecules/LoginForm'
+import SignupForm from '../molecules/SignupForm'
+
+export default function Auth() {
+  const [isLoginForm, setLoginForm] = useState(false)
+
+  function toggleForm(){
+    setLoginForm(!isLoginForm)
+  }
+
+  return (
+    <div>
+      <h2>
+        Klaytn-based NFT<br />
+        photo licensing application
+      </h2>
+      <h1>
+        <img src='./logo-klaystagram.png' alt='Klaystagram' />
+      </h1>
+      {isLoginForm ? <LoginForm /> : <SignupForm />}
+      <p>
+        {isLoginForm ? `Don't have an account?` : `Have an account?`}
+        <span onClick={toggleForm}>
+          {isLoginForm ? `Sign up` : `Login`}
+        </span>
+      </p>
+    </div>
+  )
+}
