a7412c59a51c8d398bd00632837097f41d4d5d26
jthcast <jthcast@gmail.com>
2021-06-13 16:04:08 +0900

    Add crypto util

---
+++ utils/crypto.ts
@@ -0,0 +1,22 @@
+import caver from '../klaytn/caver'
+
+export const isValidAddress = (address: string) => caver.utils.isaddress(address)
+
+export const isValidPrivateKey = (privateKey: string) => {
+  const washedPrivateKey = privateKey.slice(0, 2) === `0x`
+    ? privateKey.slice(2)
+    : privateKey
+
+  return washedPrivateKey
+    .toString()
+    .split(``)
+    .filter((character: string) => /^[a-f0-9A-F]$/i.test(character))
+    .length === 64
+}
+
+export const getWallet = () => {
+  if(!caver.klay.accounts.wallet.length){
+    return null
+  }
+  return caver.klay.accounts.wallet[0]
+}
