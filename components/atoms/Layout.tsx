3bf55d381c6fe87a67d18047a36f19bedf8da20f
jthcast <jthcast@gmail.com>
2021-06-14 00:41:52 +0900

    Fix add modal to layout

--- components/atoms/Layout.tsx
+++ components/atoms/Layout.tsx
@@ -1,8 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../redux/modules'
+import { hideModal } from '../../redux/modules/ui'
+import Modal from './Modal'
+
 export default function Layout({ children }) {
+  const dispatch = useDispatch()
+  const { isOpen = false, content } = useSelector((state: RootState) => state.ui.modal) || {}
+
+  function modalHandling(){
+    dispatch(hideModal())
+  }
 
   return (
     <>
       <main>{children}</main>
+      <Modal
+        isOpen={isOpen}
+        openHandler={modalHandling}
+      >
+        {content}
+      </Modal>
     </>
   )
 }
