1c8190b082aecc701009263d1d775f642905f0c6
jthcast <jthcast@gmail.com>
2021-06-14 15:16:55 +0900

    Fix add toast and ui

--- components/atoms/Layout.tsx
+++ components/atoms/Layout.tsx
@@ -1,14 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../redux/modules'
 import { hideModal } from '../../redux/modules/ui'
+import ui from '../../utils/ui'
+import Toast from '../molecules/Toast'
 import Modal from './Modal'
 
 export default function Layout({ children }) {
   const dispatch = useDispatch()
   const { isOpen = false, content } = useSelector((state: RootState) => state.ui.modal) || {}
+  const toast = useSelector((state: RootState) => state.ui.toast)
 
   function modalHandling(){
-    dispatch(hideModal())
+    ui.hideModal()
   }
 
   return (
@@ -20,6 +23,7 @@ export default function Layout({ children }) {
       >
         {content}
       </Modal>
+      <Toast toast={toast} />
     </>
   )
 }
