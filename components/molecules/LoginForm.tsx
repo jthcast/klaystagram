0532f0de6f810e73e9ed0108a7af951de10724d1
jthcast <jthcast@gmail.com>
2021-06-16 18:16:13 +0900

    Fix div to form and add css

--- components/molecules/LoginForm.tsx
+++ components/molecules/LoginForm.tsx
@@ -4,6 +4,8 @@ import { isValidPrivateKey } from '../../utils/crypto'
 import { login } from '../../redux/modules/auth'
 import Input from '../atoms/Input'
 import Button from '../atoms/Button'
+import { css } from '@emotion/css'
+import globalCss from '../../styles/global-css'
 
 export default function LoginForm() {
   const dispatch = useDispatch()
@@ -14,7 +16,9 @@ export default function LoginForm() {
     setPrivateKey(event.target.value)
   }
 
-  function loginHandling(){
+  function loginHandling(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+
     if(isValidPrivateKey(privateKey)){
       dispatch(login(privateKey))
     }else{
@@ -23,26 +27,42 @@ export default function LoginForm() {
   }
 
   return (
-    <div>
-      <Input 
-        // className=''
+    <form className={cssContainer} onSubmit={loginHandling}>
+      <Input
+        isRequire
         type='password'
         label='Login with Private Key'
         placeholder='0x2c4078447...'
         onChange={onChangeHandling}
       />
       <Button
-        // className=''
+        className={cssButton}
         title='Login'
-        onClick={loginHandling}
+        type='submit'
       >
         Log in
       </Button>
       {warningMessage && 
-        <p>
+        <p className={cssWarningMessage}>
           {warningMessage}
         </p>
       }
-    </div>
+    </form>
   )
 }
+
+const cssContainer = css`
+  width: 100%;
+  display: grid;
+  gap: 1rem;
+`
+
+const cssButton = css`
+  font-weight: bold;
+  padding: 0.5rem 0;
+`
+
+const cssWarningMessage = css`
+  font-size: 0.9rem;
+  color: ${globalCss.color.danger};
+`
