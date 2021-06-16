f4338779d52f4fbf25410d60ccdd531d2889f0fc
jthcast <jthcast@gmail.com>
2021-06-17 01:37:10 +0900

    Fix PhotoHeader css

--- components/molecules/PhotoHeader.tsx
+++ components/molecules/PhotoHeader.tsx
@@ -1,4 +1,6 @@
+import { css } from '@emotion/css'
 import { KLAYTN_SCOPE } from '../../constants/url'
+import globalCss from '../../styles/global-css'
 import LinkNewTab from '../atoms/LinkNewTab'
 
 interface IPhotoHeader {
@@ -12,14 +14,32 @@ export default function PhotoHeader({
 }: IPhotoHeader) {
 
   return (
-    <header>
+    <header className={cssContainer}>
       <LinkNewTab
+        className={cssCurrentOwner}
         link={`${KLAYTN_SCOPE}account/${currentOwner}`}
         title={currentOwner}
       />
-      <p>
+      <p className={cssLoation}>
         {location}
       </p>
     </header>
   )
 }
+
+const cssContainer = css`
+  background-color: ${globalCss.color.backgroundColor};
+  border-left: 0.1rem solid ${globalCss.color.borderColor};
+  border-bottom: 0.1rem solid ${globalCss.color.borderColor};
+  overflow-wrap: anywhere;
+  padding: 1rem;
+`
+
+const cssCurrentOwner = css`
+  font-weight: bold;
+  text-decoration: none;
+`
+
+const cssLoation = css`
+  font-size: 0.75rem;
+`
