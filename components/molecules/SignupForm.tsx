86f76f0c78ee6d2c06f3482dc23c3318d4b4a02c
jthcast <jthcast@gmail.com>
2021-06-16 18:25:59 +0900

    Fix div to form and add css

--- components/molecules/SignupForm.tsx
+++ components/molecules/SignupForm.tsx
@@ -2,31 +2,44 @@ import { useState } from 'react'
 import caver from '../../klaytn/caver'
 import Input from '../atoms/Input'
 import Button from '../atoms/Button'
+import { css } from '@emotion/css'
 
 export default function SignupForm() {
   const [privateKey, setPrivateKey] = useState(``)
 
-  function generatePrivateKey(){
+  function generatePrivateKey(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+
     const { privateKey } = caver.klay.accounts.create()
     setPrivateKey(privateKey)
   }
 
   return (
-    <div>
-      <Input 
-        // className=''
+    <form className={cssContainer} onSubmit={generatePrivateKey}>
+      <Input
         label='Private Key'
         placeholder='Generate Private Key to Sign up'
         value={privateKey || ''}
         isReadOnly
       />
       <Button
-        // className=''
+        className={cssButton}
         title='Generate Private Key'
-        onClick={generatePrivateKey}
+        type='submit'
       >
         Generate Private Key
       </Button>
-    </div>
+    </form>
   )
 }
+
+const cssContainer = css`
+  width: 100%;
+  display: grid;
+  gap: 1rem;
+`
+
+const cssButton = css`
+  font-weight: bold;
+  padding: 0.5rem 0;
+`
