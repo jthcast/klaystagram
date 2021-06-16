5de7eb3a88904be4226fa34c49d2b7f17dea1724
jthcast <jthcast@gmail.com>
2021-06-17 01:50:49 +0900

    Fix PhotoInfo css

--- components/atoms/PhotoInfo.tsx
+++ components/atoms/PhotoInfo.tsx
@@ -1,3 +1,6 @@
+import { css } from '@emotion/css'
+import globalCss from '../../styles/global-css'
+
 interface IPhotoInfo {
   name: string
   issueDate?: string
@@ -11,10 +14,27 @@ export default function PhotoInfo({
 }: IPhotoInfo) {
 
   return (
-    <>
-      <h2>{name}</h2>
+    <div className={cssContainer}>
+      <h2 className={cssFileName}>{name}</h2>
       <p>{caption}</p>
-      <span>{issueDate}</span>
-    </>
+      <span className={cssIssueDate}>{issueDate}</span>
+    </div>
   )
 }
+
+const cssContainer = css`
+  padding: 1rem;
+  background-color: ${globalCss.color.backgroundColor};
+  border-left: 0.1rem solid ${globalCss.color.borderColor};
+  border-bottom: 0.1rem solid ${globalCss.color.borderColor};
+  height: 100%;
+`
+
+const cssFileName = css`
+  font-size: 1rem;
+`
+
+const cssIssueDate = css`
+  color: ${globalCss.color.colorDown};
+  font-size: 0.75rem;
+`
