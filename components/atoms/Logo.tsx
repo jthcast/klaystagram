3bd99952ff8a02ec14303248a1b0eaa6bcfca551
jthcast <jthcast@gmail.com>
2021-06-16 17:32:34 +0900

    Add Logo component

---
+++ components/atoms/Logo.tsx
@@ -0,0 +1,37 @@
+import { css, cx } from '@emotion/css'
+import Icon from './Icon'
+
+interface ILogo{
+  className?: string
+}
+
+export default function Logo({
+  className
+}: ILogo){
+
+  return(
+    <div className={cx(
+      { [cssLogo]: true },
+      { [className]: !!className },
+    )}>
+      <Icon iconName='klaytnLogo' />
+      <h1>
+        Klaystagram
+      </h1>
+    </div>
+  )
+}
+
+const cssLogo = css`
+  display: flex;
+  align-items: center;
+
+  svg{
+    font-size: 1.4em;
+    margin: 0.2rem 0.25rem 0 0;
+  }
+  
+  h1{
+    font-size: 1.25rem;
+  }
+`
