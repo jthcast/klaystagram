13a8c1d6d0ecbec904cdea459626c7e7babc07d8
jthcast <jthcast@gmail.com>
2021-06-16 15:30:26 +0900

    Fix show feed without login state

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,12 @@ import { useSelector } from 'react-redux'
 import Layout from "../components/atoms/Layout";
 import Auth from '../components/organisms/Auth'
 import Feed from '../components/organisms/Feed'
-import { RootState } from '../redux/modules'
 
 export default function Home(){
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
   return (
     <Layout>
-      {/* {isLoggedIn && <Nav />} */}
-      {isLoggedIn ? <Feed /> : <Auth />}
+      <Feed />
     </Layout>
   )
 }
