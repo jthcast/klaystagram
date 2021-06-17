960eb8522837691dc3174f9419aed1580f864e1a
jthcast <jthcast@gmail.com>
2021-06-17 16:32:50 +0900

    Fix Textarea css

--- components/atoms/Textarea.tsx
+++ components/atoms/Textarea.tsx
@@ -1,5 +1,6 @@
-import { cx } from '@emotion/css'
+import { css, cx } from '@emotion/css'
 import { TextareaHTMLAttributes } from 'react'
+import globalCss from '../../styles/global-css'
 
 interface ITextarea extends TextareaHTMLAttributes<HTMLElement>{
   className?: string
@@ -21,11 +22,17 @@ export default function Textarea({
   
   return (
     <label className={cx(
-      // { [cssButton]: true },
-      { [className]: className ? true : false }
+      { [cssLabel]: true },
+      { [className]: !!className }
     )}>
-      {label}
+      <span className={cssLabelSpan}>
+        {label}
+      </span>
       <textarea
+        className={cx(
+          { [cssTextarea]: true },
+          { [cssLabelText]: !!label }
+        )}
         autoComplete='off'
         required={isRequire}
         onChange={onChange}
@@ -35,3 +42,48 @@ export default function Textarea({
     </label>
   )
 }
+
+const cssLabel = css`
+  position: relative;
+  border: 1px solid ${globalCss.color.borderColor};
+  border-radius: 0.25rem;
+  
+  &:focus-within{
+    outline: auto;
+    border: 1px solid ${globalCss.color.secondaryBrandColor};
+    color: ${globalCss.color.secondaryBrandColor};
+  }
+`
+
+const cssLabelText = css`
+  margin-top: 2.4rem;
+`
+
+const cssLabelSpan = css`
+  position: absolute;
+  font-size: 0.8rem;
+  margin: 0.8rem 1.1rem;
+  pointer-events: none;
+`
+
+const cssTextarea = css`
+  background-color: transparent;
+  color: ${globalCss.color.color};
+  border: 0;
+  width: 100%;
+  padding: 0 1.1rem 0.8rem 1.1rem;
+  font-size: 1rem;
+  resize: none;
+
+  &:disabled{
+    cursor: not-allowed;
+  }
+
+  &:focus{
+    outline: none;
+  }
+
+  &::placeholder{
+    color: ${globalCss.color.borderColor};
+  }
+`
